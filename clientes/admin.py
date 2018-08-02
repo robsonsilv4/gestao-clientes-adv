6ce92e72fd7ce835855421be890ab333eb1b5086
robsonsilv4 <robsonsilv410@gmail.com>
2018-08-02 07:46:54 -0300

    Habilitando filtros

--- clientes/admin.py
+++ clientes/admin.py
@@ -11,10 +11,16 @@ class PessoaAdmin(admin.ModelAdmin):
 
     # fields = [('first_name', 'last_name'), ('age', 'salary'), 'bio', 'photo']
     # exclude = ['bio']
-    list_display = ['first_name', 'last_name', 'age', 'salary', 'photo']
+    # list_display = ['first_name', 'last_name', 'age', 'salary', 'photo']
+
+    list_filter = ('age', 'salary', 'num_doc__num_doc')
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_filter = ('preco',)
 
 
 admin.site.register(Pessoa, PessoaAdmin)
 admin.site.register(Documento)
 admin.site.register(Venda)
-admin.site.register(Produto)
+admin.site.register(Produto, ProdutoAdmin)
