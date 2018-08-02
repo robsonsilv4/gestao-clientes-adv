9451fb85a5bdc4f9241dc7b3b075afe619a9bb3f
robsonsilv4 <robsonsilv410@gmail.com>
2018-08-02 07:11:16 -0300

    Personalizando a exibição de fields

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Pessoa, Documento, Produto, Venda
 
-admin.site.register(Pessoa)
+
+class PessoaAdmin(admin.ModelAdmin):
+    # fields = ['first_name', 'last_name', 'age', 'salary']
+    exclude = ['bio']
+    list_display = ['id', 'first_name', 'last_name', 'age', 'salary', 'photo']
+
+
+admin.site.register(Pessoa, PessoaAdmin)
 admin.site.register(Documento)
 admin.site.register(Venda)
 admin.site.register(Produto)
