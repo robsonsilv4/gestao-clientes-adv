a46858ff9e1317dfe5271c5aedb805d486ab3cfd
rosbonsilv4 <robsonsilv410@gmail.com>
2018-08-07 08:51:37 -0300

    Criando Dashboard e utilizando AVG

--- vendas/urls.py
+++ vendas/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import DashboardView
 
 urlpatterns = [
-    # pass
+    path('dashboard/', DashboardView.as_view(), name='dashboard')
 ]
--- vendas/views.py
+++ vendas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Venda, ItemDoPedido
+from django.db.models import F, Sum, Avg, Max, FloatField
 
-# Create your views here.
+
+class DashboardView(View):
+    def get(self, request):
+        media = Venda.objects.all().aggregate(Avg('valor'))['valor__avg']
+        return render(request, 'vendas/dashboard.html', {'media': media})
