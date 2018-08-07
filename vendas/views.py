d9e6c89c1cc3557cbbdc931c93fa6932c435bcdb
rosbonsilv4 <robsonsilv410@gmail.com>
2018-08-07 09:10:41 -0300

    Min, Max e Count

--- vendas/views.py
+++ vendas/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Venda, ItemDoPedido
-from django.db.models import F, Sum, Avg, Max, FloatField
+from django.db.models import F, Sum, Avg, Min, Max, Count
 
 
 class DashboardView(View):
     def get(self, request):
-        media = Venda.objects.all().aggregate(Avg('valor'))['valor__avg']
-        return render(request, 'vendas/dashboard.html', {'media': media})
+        data = {'media': Venda.objects.all().aggregate(Avg('valor'))['valor__avg'],
+                'media_desc': Venda.objects.all().aggregate(Avg('desconto'))['desconto__avg'],
+                'min': Venda.objects.all().aggregate(Min('valor'))['valor__min'],
+                'total': Venda.objects.all().aggregate(Count('id'))['id__count'],
+                'total_nfe': Venda.objects.filter(nfe_emitida=True).aggregate(Count('id'))['id__count']}
+        return render(request, 'vendas/dashboard.html', data)
