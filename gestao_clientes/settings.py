a435f1e6914c9f93295997c9ef7dfc37ae17cc8e
rosbonsilv4 <robsonsilv410@gmail.com>
2018-08-09 15:21:15 -0300

    Criando Middleware

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -55,7 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'mymiddlewares.MetaData.MetaData',
 ]
 
 ROOT_URLCONF = 'gestao_clientes.urls'
