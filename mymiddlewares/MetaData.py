98b73939d26dfc25638954a62215875025e5b3b6
rosbonsilv4 <robsonsilv410@gmail.com>
2018-08-10 14:10:09 -0300

    Corrigido local dos middlewares

---
+++ mymiddlewares/MetaData.py
@@ -0,0 +1,19 @@
+class MetaData(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        return response
+
+
+def process_view(self, request, view_func, view_args, view_kwargs):
+    if request.user.is_authenticated:
+        message = 'Seja bem-vindo %s' % request.user.username
+    else:
+        message = 'Olá, bom dia'
+
+    request.session['message'] = message
+
+    return None
