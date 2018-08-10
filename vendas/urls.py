1fc327fba29ec10ce885ff82028ce695f3e0b543
rosbonsilv4 <robsonsilv410@gmail.com>
2018-08-10 16:23:00 -0300

    Mestre detalhes com Forms manuais

--- vendas/urls.py
+++ vendas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DashboardView
+from .views import DashboardView, NovaVenda
 
 urlpatterns = [
+    path('nova-venda/', NovaVenda.as_view(), name='nova-venda'),
     path('dashboard/', DashboardView.as_view(), name='dashboard')
 ]
--- vendas/views.py
+++ vendas/views.py
@@ -19,3 +19,27 @@ class DashboardView(View):
                 'ped_total': Venda.objects.pedido_total,
                 'ped_tot_nfe': Venda.objects.pedido_total_nfe}
         return render(request, 'vendas/dashboard.html', data)
+
+
+class NovaVenda(View):
+    def get(self, request):
+        return render(request, 'vendas/nova-venda.html')
+
+    def post(self, request):
+        data = {'numero': request.POST['numero'],
+                'desconto': float(request.POST['numero']),
+                'venda': request.POST['venda_id']}
+
+        if data['venda']:
+            venda = Venda.objects.get(id=data['venda'])
+        else:
+            venda = Venda.objects.create(
+                numero=data['numero'],
+                desconto=data['desconto']
+            )
+
+        itens = venda.itemdopedido_set.all()
+        data['venda_obj'] = venda
+        data['itens'] = itens
+
+        return render(request, 'vendas/nova-venda.html', data)
