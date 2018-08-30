054598be6d202552da9b571399a42c84dc245335
rosbonsilv4 <robsonsilv410@gmail.com>
2018-08-30 07:40:17 -0300

    Testando total do pedido e descontos

--- vendas/test/test_vendas.py
+++ vendas/test/test_vendas.py
@@ -1,10 +1,24 @@
 from django.test import TestCase
-from vendas.models import Venda
+from gestao_clientes.vendas.models import Venda, ItemDoPedido
+from gestao_clientes.produtos.models import Produto
 
 
 class VendaTestCase(TestCase):
     def setUp(self):
-        Venda.objects.create(numero='123')
+        self.venda = Venda.objects.create(numero=123, desconto=10)
+        self.produto = Produto.objects.create(
+            descricao='Produto 1', preco=10)
 
-    def test_verifica_numero_vendas(self):
-        assert Venda.objects.all().count() == 2
+    def test_numero_vendas(self):
+        assert Venda.objects.all().count() == 1
+
+    def test_valor_venda(self):
+        ItemDoPedido.objects.create(
+            venda=self.venda,
+            produto=self.produto,
+            quantidade=10,
+            desconto=10)
+        assert self.venda.valor == 80
+
+    def test_desconto(self):
+        assert self.venda.desconto == 10
